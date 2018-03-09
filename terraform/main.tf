2e386e99b54cda431f9a57cda9f37295d86d9ef5
Bobby DeVeaux <me@bobbyjason.co.uk>
2018-03-09 18:14:18 +0000

    adding terraform remote s3 storage for state file + dynamoDB for lock state

--- terraform/main.tf
+++ terraform/main.tf
@@ -408,3 +408,48 @@ resource "aws_db_instance" "default" {
   skip_final_snapshot  = true
   final_snapshot_identifier = "deletemeplz"
 }
+
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+    bucket = "bobbyjason-tf-remote-state-storage-s3"
+    region = "eu-west-2"
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+    tags {
+      Name = "S3 Remote Terraform State Store"
+    }      
+}
+
+
+terraform {
+ backend "s3" {
+ encrypt = true
+ bucket = "bobbyjason-tf-remote-state-storage-s3"
+ region = "eu-west-2"
+ dynamodb_table = "bobbyjason-tf-state-lock-dynamo"
+ key = "state"
+ }
+}
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "bobbyjason-tf-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
